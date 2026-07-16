d8445a913c45c852e69eba595b6a614a22a8aa54
Aryan Sharma <aryanhere10@gmail.com>
2026-07-16 13:16:13 +0530

    app.get

--- express/index.js
+++ express/index.js
@@ -9,7 +9,15 @@ app.listen(port, () => {
     console.log(`App is running on port number ${port}`);
 });
 
-app.use((req, res) => {
-    console.log("Request received");
-    res.send("Here is the response");
+app.get("/", (req, res) => {
+    res.send("Contacted Root -> Path");
+});
+app.get("/search", (req, res) => {
+    res.send("Contacted Search Path");
+});
+app.get("/game", (req, res) => {
+    res.send("Contacted Game Path");
+});
+app.get('*any', (req, res) => {
+    res.send("ERROR : Unidentified Path");
 });
